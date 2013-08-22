5ad2a65585a3e05f7adf2702b0fad0ee4ceb6f82
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-22 02:18:53 +0200

    Link in custom secret token

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,5 +9,13 @@ set :repository,  "git@github.com:michiels/statsboard.git"
 
 server "intercityup.com", :web, :app, :db, primary: true
 
+after "deploy:finalize_update", "deploy:link_secret_token"
+
+namespace :deploy do
+  task :link_secret_token do
+    run "rm -f #{release_path}/config/initializers/secret_token.rb && ln -nfs #{shared_path}/config/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
+  end
+end
+
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
