7146ec06f4f607fe74b18a4fb0bb3506ff7a3778
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-22 02:10:17 +0200

    Add gem downloads

--- metrics/metrics.rb
+++ metrics/metrics.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class DashboardMetrics < MetricsDefinition
 
   metric "locomotive-chef-repo stargazers" do |m|
@@ -5,6 +7,24 @@ class DashboardMetrics < MetricsDefinition
 
     m.fetch_with(lambda {
       return Octokit.repo('intercity/locomotive-chef-repo').watchers_count
+    })
+  end
+
+  metric "locomotive-chef-repo forks" do |m|
+    m.reason = "Forks might indicate usage of the chef recipes"
+
+    m.fetch_with(lambda {
+      return Octokit.repo('intercity/locomotive-chef-repo').network_count
+    })
+  end
+
+  metric "intercity 0.0.1 gem downloads" do |m|
+    m.reason = "Intercity gem downloads indicate usage for deployment"
+
+    m.fetch_with(lambda {
+      url = open('http://www.rubygems.org/api/v1/downloads/intercity-0.0.1.json')
+      json = url.read
+      JSON.parse(json)['version_downloads']
     })
   end
 
