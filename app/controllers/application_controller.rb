b46e0f1315a3a358c89abca65e23db6176590e34
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-23 17:09:19 +0200

    Added ip security for create and destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :secure_ips, only: [:create, :destroy]
+
+  protected
+
+  def secure_ips
+    if !%w(127.0.0.1).include?(request.remote_ip)
+      render nothing: true, status: 403, layout: false
+    end
+  end
 end
